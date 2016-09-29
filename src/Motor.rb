315ce13271942b188f61d94b4a3c1f9d8ff1295a
brianwolf <brian.d.lobo@gmail.com>
2016-09-29 14:04:35 -0300

    pequeño refactor en motor

--- src/Motor.rb
+++ src/Motor.rb
@@ -5,50 +5,50 @@ require_relative 'Manejo_Resultados'
 require_relative 'Test_tadp'
 
 class Motor
-  include Parser, Manejo_Resultados
+  include Parser,
+          Manejo_Resultados
 
-  attr_accessor :lista_de_test
+  @lista_de_test
+  @clases_cargadas
 
   def initialize (*clases_test)
-    crear_y_cargar_objetos_test clases_test
+    @clases_cargadas = clases_test.select {|clase| es_un_test_suite? clase}
   end
 
   def testear(*args)
-    limpiar_test_cargados
+    @lista_de_test = []
 
-    test_a_correr = generar_lista_de_test_a_partir_de_argumentos args
+    cargar_lista_de_test_a_partir_de_argumentos args
 
-    ejecutar_tests test_a_correr
-    mostrar_resultados test_a_correr
+    ejecutar_tests_cargados
+    mostrar_resultados_de_test_cargados
 
-    obtener_resultados_de_tests test_a_correr
+    obtener_resultados_de_tests_cargados
   end
 
+  private
   def esta_cargado? suite
-    self.lista_de_test.any? { |test| test.suite == suite}
+    @clases_cargadas.any? { |clase_suite| clase_suite == suite}
   end
 
   private
-  def limpiar_test_cargados
-    self.lista_de_test.each { |test| test.resultado = nil }
+  def mostrar_resultados_de_test_cargados
+    mostrar_resultados @lista_de_test
   end
 
   private
-  def crear_y_cargar_objetos_test (clases_test)
-    self.lista_de_test ||=[]
+  def crear_y_cargar_objetos_test (suite)
 
-    clases_test.each { |suite|
-      (filtrar_metodos_test suite.instance_methods).each { |test|
-        self.lista_de_test << Test_tadp.new(suite, test)
-      }
+    (filtrar_metodos_test suite.instance_methods).each { |test|
+      @lista_de_test << Test_tadp.new(suite, test)
     }
   end
 
   private
-  def obtener_resultados_de_tests lista_test
+  def obtener_resultados_de_tests_cargados
     @resultados_obtenidos = []
 
-    lista_test.each{|test|
+    @lista_de_test.each{|test|
       @resultados_obtenidos << test.resultado if test.te_ejecutaste?
     }
 
@@ -56,26 +56,26 @@ class Motor
   end
 
   private
-  def generar_lista_de_test_a_partir_de_argumentos args
+  def cargar_lista_de_test_a_partir_de_argumentos args
     case
       when args.count == 0
-        @test_a_correr = self.lista_de_test
+        @clases_cargadas.each{|suite| crear_y_cargar_objetos_test suite}
 
       when args.count == 1 && esta_cargado?(args[0])
-        @test_a_correr = self.lista_de_test.select{|test| test.suite == args[0]}
+        crear_y_cargar_objetos_test args[0]
 
       when args.count > 1
-        metodos_test = filtrar_metodos_test args[1..-1]
-        @test_a_correr = self.lista_de_test.select{|test|
-          (test.suite == args[0]) && (metodos_test.include? test.test)}
+        suite = args[0]
+        metodos_test = (filtrar_metodos_test args[1..-1]).select{|metodo| suite.new.respond_to? metodo}
+
+        metodos_test.each {|test| @lista_de_test << Test_tadp.new(suite, test)}
     end
 
-    @test_a_correr
   end
 
   private
-  def ejecutar_tests test_a_correr
-    test_a_correr.each { |test| test.testear}
+  def ejecutar_tests_cargados
+    @lista_de_test.each { |test| test.testear}
   end
 
 end
